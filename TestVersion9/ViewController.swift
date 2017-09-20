baca061d7b467d966f7eca85eb9169e01fe53282
BraverChiang <huanfa2020@gmail.com>
2017-09-20 10:05:10 +0800

    添加一个按钮

--- TestVersion9/ViewController.swift
+++ TestVersion9/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func myBtn(_ sender: UIButton) {
+    }
     @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
