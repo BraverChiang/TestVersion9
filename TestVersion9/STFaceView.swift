8f53819e0a98648ce08a6f0dd436ddfa2c79b8de
BraverChiang <huanfa2020@gmail.com>
2017-09-23 18:27:06 +0800

    A1.2 修改表情 (代码方式)

--- TestVersion9/STFaceView.swift
+++ TestVersion9/STFaceView.swift
@@ -9,20 +9,34 @@
 import UIKit
 
 class STFaceView: UIView {
-
-    override func draw(_ rect: CGRect) {
-        //半径
-        let skullRadius = min(bounds.size.width, bounds.size.height)/2
-        //圆心
-        let skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)
+    
+    
+    var scale: CGFloat = 0.9
+    //该全局的变量就全局化
+    private var skullRadius: CGFloat{
+        //return 表示初始化一个默认值
+        return min(bounds.size.width, bounds.size.height)/2 * scale
+    }
+    private var skullCenter: CGPoint {
+        //return 表示初始化一个默认值
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    
+    private func pathForSkull()  -> UIBezierPath{
         //画圆圈
         let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: CGFloat(2 * Double.pi), clockwise: false)
         //边框宽度
         path.lineWidth = 5.0
+        return path
+    }
+    
+
+    override func draw(_ rect: CGRect) {
         //边框颜色
         UIColor.blue.set()
-        path.stroke()
+        pathForSkull().stroke()
     }
  
 
 }
+
