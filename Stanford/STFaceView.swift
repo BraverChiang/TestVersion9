35455cc8f71f26da42d8fb191aa509d2e6d5c5db
BraverChiang <huanfa2020@gmail.com>
2017-09-23 19:12:54 +0800

    B2.1 新增眼睛 (Struct, Enum, 内部方法)

--- Stanford/STFaceView.swift
+++ Stanford/STFaceView.swift
@@ -22,6 +22,41 @@ class STFaceView: UIView {
         return CGPoint(x: bounds.midX, y: bounds.midY)
     }
     
+    //眼睛嘴巴相对脸部的比例
+    private struct Ratios{
+        static let skullRadiusToEyeOffset: CGFloat = 3
+        static let skullRadiusToEyeRadius: CGFloat = 10
+        static let skullRadiusToMouthWith: CGFloat = 1
+        static let skullRadiusToMouthHeight: CGFloat = 3
+        static let skullRadiusToMouthOffset: CGFloat = 3
+    }
+    //左眼和右眼
+    private enum Eye{
+        case left
+        case right
+    }
+    
+    //画眼睛边框
+    private func pathForEye(_ eye: Eye) -> UIBezierPath {
+        
+        //眼睛中心: 内部方法##
+        func centerOfEye(_ eye: Eye) -> CGPoint {
+            let eyeOffset = skullRadius / Ratios.skullRadiusToEyeOffset
+            var eyeCenter = skullCenter
+            eyeCenter.y -= eyeOffset
+            eyeCenter.x += ((eye == .left) ? -1 : 1)*eyeOffset
+            return eyeCenter
+        }
+        //眼睛半径:
+        let eyeRadius = skullRadius / Ratios.skullRadiusToEyeRadius
+        let eyeCenter = centerOfEye(eye)
+        //画眼睛边框
+        let eyePath = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: false)
+        eyePath.lineWidth = 5.0
+        return eyePath
+        
+    }
+    
     private func pathForSkull()  -> UIBezierPath{
         //画圆圈
         let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: CGFloat(2 * Double.pi), clockwise: false)
@@ -35,6 +70,8 @@ class STFaceView: UIView {
         //边框颜色
         UIColor.green.set()
         pathForSkull().stroke()
+        pathForEye(.left).stroke()
+        pathForEye(.right).stroke()
     }
  
 
