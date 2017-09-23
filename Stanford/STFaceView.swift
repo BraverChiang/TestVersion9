d4d65feee5d29c7f07ddbf6894d94367f538a411
BraverChiang <huanfa2020@gmail.com>
2017-09-23 19:29:15 +0800

    B2.3 修改注释 (//MARK:)

--- Stanford/STFaceView.swift
+++ Stanford/STFaceView.swift
@@ -37,7 +37,7 @@ class STFaceView: UIView {
         case right
     }
     
-    //画眼睛边框
+    //MARK:画眼睛边框
     private func pathForEye(_ eye: Eye) -> UIBezierPath {
         
         //眼睛中心: 内部方法##
@@ -68,15 +68,16 @@ class STFaceView: UIView {
         
     }
     
+    //MARK:画面部边框
     private func pathForSkull()  -> UIBezierPath{
-        //画圆圈
+        
         let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: CGFloat(2 * Double.pi), clockwise: false)
         //边框宽度
         path.lineWidth = 5.0
         return path
     }
     
-
+    //MARK: 核心方法draw
     override func draw(_ rect: CGRect) {
         //边框颜色
         UIColor.green.set()
